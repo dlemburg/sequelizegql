469f8ff75bfc10434c546fa57ea1590832181420
danielMadeRenovation <daniel@maderenovation.com>
2021-12-04 14:28:36 -0800

    removing hardcoded legacy code

--- src/util/build-schema-util.ts
+++ src/util/build-schema-util.ts
@@ -3,7 +3,7 @@ import { merge } from 'lodash';
 export const buildCustomSchema = ({ models: Models, customSchemaPath }) => {
   let result = { schemaMap: {}, resolvers: {}, typedefs: [] as any[] };
 
-  for (let name of [...Object.keys(Models), 'Health', 'Lookups', 'Customer']) {
+  for (let name of [...Object.keys(Models)]) {
     try {
       const exports = require(`${__dirname}/${customSchemaPath}/${name}`);
 
