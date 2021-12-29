fe7712b48929ce6a4ac53143da0d2e211d244206
dlemburg <daniel.lemburg1@gmail.com>
2021-12-29 13:35:24 -0800

    updating example impl

--- examples/library/src/graphql/schemas/index.ts
+++ examples/library/src/graphql/schemas/index.ts
@@ -1,19 +1,22 @@
 import SequelizeGraphql from '../../../../../src';
+import { GeneratedResolverField } from '../../../../../src/types';
 
 import * as Enums from '../../orm/enums';
 import * as Models from '../../orm/models';
+
+const schemaMap = {
+  BookAuthor: {
+    resolvers: {
+      [GeneratedResolverField.CREATE_MUTATION]: { generate: false },
+    },
+  },
+};
 
 export const getSchema = async () => {
   const graphqlSequelize = SequelizeGraphql.initialize({
     enums: Enums,
     models: Models,
-    schemaMap: {
-      BookAuthor: {
-        resolvers: {
-          create: { generate: false },
-        },
-      },
-    },
+    schemaMap,
   } as any);
 
   return {
