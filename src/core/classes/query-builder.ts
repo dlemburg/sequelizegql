999b53203feb18766841ea97e212a8c724d7f392
dlemburg <daniel.lemburg1@gmail.com>
2022-01-11 14:14:10 -0800

    small refactorings

--- src/core/classes/query-builder.ts
+++ src/core/classes/query-builder.ts
@@ -23,6 +23,8 @@ type BuildIncludeInput = {
 
 type FieldIntrospectionTuple = [string, any];
 
+const BASE_ACC = () => ({ attributes: [], include: undefined });
+
 const buildInclude = ({ association, include, attributes = [] }: BuildIncludeInput) => {
   return [
     {
@@ -48,64 +50,63 @@ const recurseQueryFields = (
   //   return !result?.length && !models?.[result[0]];
   // });
 
-  const result = fieldEntries?.reduce(
-    (acc, [key, value]: [string, any]) => {
-      const associationValue = modelAttributes?.associations?.[key];
-      const attributeValue = modelAttributes?.[key];
-      const where = parseWhere(value?.fieldsByTypeName?.args?.where, modelMapOptions);
+  const result = fieldEntries?.reduce((acc, [key, value]: [string, any]) => {
+    const associationValue = modelAttributes?.associations?.[key];
+    const attributeValue = modelAttributes?.[key];
+    const where = parseWhere(value?.fieldsByTypeName?.args?.where, modelMapOptions);
 
-      if (attributeValue) {
-        acc.attributes.push(key);
-      } else if (associationValue?.type) {
-        const Model = associationValue?.type && models?.[associationValue?.type];
-        const fields = value?.fieldsByTypeName?.[associationValue?.type];
+    if (attributeValue) {
+      acc.attributes.push(key);
+    } else if (associationValue?.type) {
+      const Model = associationValue?.type && models?.[associationValue?.type];
+      const fields = value?.fieldsByTypeName?.[associationValue?.type];
 
-        if (Model) {
-          const nextAssociationFields = Object.entries(fields).filter(([xKey, xValue]: any) => {
-            const result = Object.keys(xValue?.fieldsByTypeName ?? {});
-            return result?.length && models?.[result[0]];
-          });
-          const attributeFields = Object.keys(fields).filter((field) => {
-            const result = nextAssociationFields.find((y) => y[0] === field);
-            return !result;
-          });
+      if (Model) {
+        const nextAssociationFields = Object.entries(fields).filter(([xKey, xValue]: any) => {
+          const result = Object.keys(xValue?.fieldsByTypeName ?? {});
+          return result?.length && models?.[result[0]];
+        });
+        const attributeFields = Object.keys(fields).filter((field) => {
+          const result = nextAssociationFields.find((y) => y[0] === field);
+          return !result;
+        });
 
-          const associationFields = nextAssociationFields.reduce(
-            (acc: any, x: FieldIntrospectionTuple) => {
-              const fieldName = x[1].name;
-              const fieldsByType = x[1].fieldsByTypeName;
-              const modelName = Object.keys(x[1]?.fieldsByTypeName ?? {})?.[0];
-              const nextQueryFields = Object.entries(fieldsByType[modelName]);
-              const nextModelAttributes = getAttributes(models[modelName])();
+        const associationFields = nextAssociationFields.reduce(
+          (accInner: any, x: FieldIntrospectionTuple) => {
+            const fieldName = x[1].name;
+            const fieldsByType = x[1].fieldsByTypeName;
+            const modelName = Object.keys(x[1]?.fieldsByTypeName ?? {})?.[0];
+            const nextQueryFields = Object.entries(fieldsByType[modelName]);
+            const nextModelAttributes = getAttributes(models[modelName])();
 
-              const { attributes: includeAttributes, include: associatedInclude } =
-                recurseQueryFields(nextQueryFields, nextModelAttributes, modelMapOptions);
+            const { attributes: includeAttributes, include: associatedInclude } =
+              recurseQueryFields(nextQueryFields, nextModelAttributes, modelMapOptions);
 
-              const baseInclude = buildInclude({
-                association: fieldName,
-                include: associatedInclude,
-                attributes: includeAttributes,
-              });
+            const baseInclude = buildInclude({
+              association: fieldName,
+              include: associatedInclude,
+              attributes: includeAttributes,
+            });
 
-              acc.include = acc?.include ? [...acc.include, ...baseInclude] : baseInclude;
+            accInner.include = accInner?.include
+              ? [...accInner.include, ...baseInclude]
+              : baseInclude;
 
-              return acc;
-            },
-            { attributes: [], include: undefined }
-          );
+            return accInner;
+          },
+          BASE_ACC()
+        );
 
-          const baseInclude = buildInclude({
-            association: key,
-            include: associationFields?.include,
-          });
-          acc.include = acc?.include ? [...acc.include, ...baseInclude] : baseInclude;
-        }
+        const baseInclude = buildInclude({
+          association: key,
+          include: associationFields?.include,
+        });
+        acc.include = acc?.include ? [...acc.include, ...baseInclude] : baseInclude;
       }
+    }
 
-      return acc;
-    },
-    { attributes: [], include: undefined } as any
-  );
+    return acc;
+  }, BASE_ACC() as any);
 
   return result;
 };
